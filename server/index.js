8d03589743917106cd329075812ff511d1245795
tomkatsman <46943896+tomkatsman@users.noreply.github.com>
2024-10-23 12:38:14 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.options('*', cors());
 
 const port = process.env.PORT || 3000;
 
-var allowed_origins = ['http://localhost', 'http://homeassistant.local:8080', 'http://vertrektijden.tomkatsman.nl'];
+var allowed_origins = ['http://localhost', 'http://homeassistant.local:8080', 'https://vertrektijden.tomkatsman.nl'];
 
 app.listen(port, () => console.log("Server running on port " + port));
 
