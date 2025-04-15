6189bc8ba159be3aac9feaa05f30c51a7df03150
tomkatsman <46943896+tomkatsman@users.noreply.github.com>
2025-04-15 12:55:07 +0200

    p

--- server/fetchApi.js
+++ server/fetchApi.js
@@ -3,8 +3,8 @@ const puppeteer = require('puppeteer');
 async function fetchDepartures() {
   const browser = await puppeteer.launch({
     headless: true,
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    executablePath: '/opt/render/.cache/puppeteer/chrome/linux-127.0.6533.88/chrome-linux64/chrome'
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+    // ❌ geen executablePath hier
   });
 
   const page = await browser.newPage();
